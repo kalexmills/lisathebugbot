358ebb27483b293490828b8ae8c713eddd8a921a
System <none>
2017-09-15 02:28:50 +0000

    Weekly roundup --- TODO: add cron job.

--- skills/bug-fixed.js
+++ skills/bug-fixed.js
@@ -1,13 +1,128 @@
-var debug = require('debug')('botkit:bug_fixed');
+var debug = require('debug')('botkit:bug-fixed');
 
 module.exports = function(controller) {
   
-  // check my squish score
+  // Record a bug count whenever the Jira integration announces a transition. 
+  controller.on('bot_message',
+                   function(bot,message) {
+    
+    console.log("Received a bot message!");
+    
+    // user ids of users who lisa will listen to... to prevent cheating this should only
+    // include the user ids of verified jira bots.
+    var allowed_bots = ["B72TB8DSA",  // BridgeBot
+                     ];
+  
+    var re = /([A-Za-z]+) ([A-Za-z]+) changed Bug (.*) from .* to "Resolved"/i;
+    
+    if(allowed_bots.includes(message.bot_id) && message.attachments[0].pretext) {
+      var match = message.attachments[0].pretext.match(re);
+      
+      console.log("Received a valid bug resolution message!");
+      
+      console.log("Name: " + match[1] + " " + match[2] + "\nLink:" + match[3]);
+      
+      // reply with a random emoji / complement combination
+      var person = match[1];
+      var complement = ["Way to go " + person + "!",
+                       "Wooooohoooo!",
+                       "Squash those bugs, " + person  + "!",
+                       "Great job, " + person + "!",
+                       "Congrats, " + person + "! How does it feel?!",
+                       "You're the best, " + person + "!"];
+
+      var emojis = ["thumbsup", 
+                    "apple", 
+                    "100", 
+                    "laughing", 
+                    "smile", 
+                    "joy",
+                    "bananadance",
+                    "clapping",
+                    "metal2",
+                    "1up",
+                    "coin",
+                    "plusone"];
+
+
+      bot.reply(message, rand(complement) + " " + ":" + rand(emojis) + ":");
+      bot.api.reactions.add({
+          name: rand(emojis) ,
+          channel: message.channel,
+          timestamp: message.ts
+      });
+
+      // store the info on the weekly leaderboards
+
+      var user_name = match[1] + " " + match[2];
+      
+      controller.storage.users.get(user_name, function(err_outer, data) {
+        var bug_record = {url : match[3], timestamp : message.ts};
+        if(data && data.bugs) {
+          // Existing user
+          
+          // Add new bug to existing array of bug records
+          if(!data.bugs.some(r => { return r.url === match[3];})) {
+            data.bugs.push(bug_record);    
+            controller.storage.users.save({id: user_name, bugs: data.bugs}, function(err_inner) {
+              if(err_inner) {
+                console.log("Could not add new bug record " + match[3] + " to user " + user_name);
+                console.log(err_inner);
+              } else {
+                console.log("Added new bug " + match[3] + " to user "  + user_name);
+              }
+            });
+          }
+        } else {
+          
+          // New user. Push new record
+          controller.storage.users.save({id:user_name, bugs: [bug_record]}, function(err_inner) {
+            if(err_inner) {
+              console.log("Could not save new bug " + match[3] + " for user "  + user_name);
+              console.log(err_inner);
+            } else {
+              console.log("Persisted new record for user " + user_name);
+            }
+          });
+        }
+      });
+    }
+  });
+  
+  
+  // User asks for a bug that Lisa remembers
+  controller.hears(['flashback', 'remember', 'past', 'reminisce'], 
+                   ['message_received', 'direct_message'], function(bot, message) {
+    bot.api.users.info({user: message.user}, (error, response) => {
+      if(response) {
+        let {name, real_name} = response.user;
+        controller.storage.users.get(real_name, (error,response) => {
+          if(response.bugs) {
+            var bug = rand(response.bugs).url;
+            var reminiscence = [
+              "Ahhh, I remember that one time when you solved " + bug + "!",
+              "I loved how you made " + bug + " go splat!",
+              "Remember " + bug + "? Those were the days!",
+              "It was great to see you work on " + bug + ".",
+              bug + ", remember that one? That was a good one!",
+              "You have no idea how much fun I had when you finished working on " + bug + "."
+            ];
+            bot.whisper(message, rand(reminiscence));
+          } else {
+            bot.whisper("Have we met? Are you a developer? I don't remember hearing about you solving any bugs. "
+                        + "Maybe I haven't been listening in the right places.");
+          }
+        });
+      }
+    });
+  });
+  
+  // User checks or mentions their squish score.
   controller.hears(['count', 'score', 'bugs', 'killcount', 'squishscore', 'number'], 
                    ['message_received','direct_message'], function(bot, message){
     bot.api.users.info({user: message.user}, (error, response) => {
       let {name, real_name} = response.user;
-      controller.storage.teams.get(real_name, (error, response) => {
+      controller.storage.users.get(real_name, (error, response) => {
         var check_action = [
             "checked my clipboard",
             "queried my database",
@@ -15,7 +130,7 @@ module.exports = function(controller) {
             "looked over my records",
             "consulted my data",
           ];
-        if(error){
+        if(error || !response || !response.bugs){
           var exhortation = [
             "Better get to work!",
             "Are you really trying?",
@@ -44,92 +159,13 @@ module.exports = function(controller) {
                               "blown-up", 
                               ":zap:",
                               ":boom:",
-                              "whacked"];
+                              "whacked"
+                             ];
           bot.whisper(message, "I've " + rand(check_action)  + " and it looks like you've " + rand(violent_verb)
-                     + " " + response.count + " bugs so far! " + rand(cheer));
+                     + " " + response.bugs.length + " bugs so far! " + rand(cheer));
         }
       });
     });
-  });
-
-  // user ids of users who lisa will listen to... to prevent cheating this should only
-  // include the user ids of verified jira bots.
-  // TODO: consider whether this feature is worth it and fix if so.
-  var allowed_bots = ["B72TB8DSA",  // BridgeBot
-                     ];
-  
-  // Record a bug count whenever the Jira integration announces a transition. This can
-  // (currently) be detected via an empty text string.
-  controller.on('bot_message',
-                   function(bot,message) {
-    
-    console.log("Received a bot message!");
-    
-    // TODO: filter all bot_message types and match message.attachment[0].pretext against this
-    // regexp
-    var re = /([A-Za-z]+) ([A-Za-z]+) changed Bug (.*) from .* to "Resolved"/i;
-    
-    if(allowed_bots.includes(message.bot_id) && message.attachments[0].pretext) {
-      var match = message.attachments[0].pretext.match(re);
-      
-      console.log("Received a valid bug resolution message!");
-      
-      // reply with a random emoji / complement combination
-      var person = match[1];
-      var complement = ["Way to go " + person + "!",
-                        "Wooooohoooo!",
-                        "Squash those bugs, " + person  + "!",
-                       "Great job, " + person + "!",
-                       "Congrats, " + person + "! How does it feel?!",
-                       "You're the best, " + person + "!"];
-
-      var emojis = ["thumbsup", 
-                    "apple", 
-                    "100", 
-                    "laughing", 
-                    "smile", 
-                    "joy",
-                    "bananadance",
-                    "clapping",
-                    "metal2",
-                    "1up",
-                    "coin",
-                    "plusone"];
-
-
-      bot.reply(message, rand(complement) + " " + ":" + rand(emojis) + ":");
-      bot.api.reactions.add({
-          name: rand(emojis) ,
-          channel: message.channel,
-          timestamp: message.ts
-      });
-
-      // store the info on the weekly leaderboards
-
-      var user_id = match[1] + " " + match[2];
-
-
-      controller.storage.teams.get(user_id, function(err_outer, data) {
-        if(err_outer) {
-          controller.storage.teams.save({id: user_id, count: 1}, function(err_inner) {
-            if(err_inner) {
-              console.log("Could not save a new bug for user " + user_id);
-              console.log(err_inner);
-            } else {
-              console.log("Persisted new record for user " + user_id);
-            }
-          });
-        } else {
-          controller.storage.teams.save({id: user_id, count: data["count"] + 1}, function(err_inner) {
-            if(err_inner) {
-              console.log("Could not update bug record for user " + user_id);
-              console.log(err_inner);
-            }
-            console.log("Incremented bug count for " + user_id + " to " + data["count"]);
-          });
-        }
-      });
-    }
   });
 
   function rand(arr) {
